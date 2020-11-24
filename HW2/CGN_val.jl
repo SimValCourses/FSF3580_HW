e381ce85543335355d6040450d7d95c91054ea3f
vlupi <lupi@kth.se>
2020-11-24 23:25:35 +0100

    Bug fix by Valerio before submission

---
+++ HW2/CGN_val.jl
@@ -0,0 +1,29 @@
+function my_cgn_val(A, b, m, x_exact, A_orig, b_orig)
+    r = copy(b);
+    p = copy(b);
+    x = zeros(length(b),1);
+    res_v = zeros(m,1);
+    err_v = zeros(m,1);
+    res_v_orig = zeros(m,1);
+    time_v = zeros(m,1);
+
+    for i = 1:m
+        t_cgn = @timed begin
+            alpha = (r'*r)/(p'*A*p);
+            x = x + alpha .*p;
+            r_old = r;
+            r = r_old - alpha .*(A*p);
+            beta = (r'*r)/(r_old'*r_old);
+            p = r + beta .*p;
+        end
+        if i == 1
+            time_v[i] = t_cgn.time;
+        else
+            time_v[i] = time_v[i-1] + t_cgn.time;
+        end
+        res_v[i]= norm(A*x-b)/norm(b)	         # residual norm
+        err_v[i]= norm(x-x_exact)/norm(x_exact); # error norm
+        res_v_orig[i]= norm(A_orig*x-b_orig)/norm(b_orig); # error norm
+    end
+    return x, res_v, err_v, time_v, res_v_orig
+end
--- HW2/hw2_ex5_val.jl
+++ HW2/hw2_ex5_val.jl
@@ -1,8 +1,9 @@
 # Exercise 5 - HW2
-Pkg.add("MAT")
+#using Pkg
+#Pkg.add("MAT")
 using MAT, PyPlot, Statistics, Random, LinearAlgebra
 include("GMRES_val.jl")
-include("CG_val.jl")
+include("CGN_val.jl")
 
 bw = MAT.matread("Bwedge2.mat")
 A = bw["B"];
@@ -12,14 +13,14 @@ c = A'*b;
 
 x_exact = A\b;
 m = 100;
-x_cgn, res_cgn, err_cgn, time_cgn = my_cg_val(As,c,m, x_exact);
+x_cgn, res_cgn, err_cgn, time_cgn, res_cgn_2 = my_cgn_val(As, c, m, x_exact, A, b)
 x_gmres, res_gmres, err_gmres, time_gmres = my_gmres_val(A,b,m, x_exact, 1e-18);
 
 pygui(true)
 figure(1)
 semilogy(1:m, res_gmres, color="black", linestyle="--",
         label=L"$\textnormal{GMRES}$")
-semilogy(1:m, res_cgn, color="red", linestyle="-",
+semilogy(1:m, res_cgn_2, color="red", linestyle="-",
         label=L"$\textnormal{CGN}$")
 xlabel(L"$\textnormal{Iteration}$")
 ylabel(L"$\frac{||Ax-b||_2}{||b||_2}$")
@@ -28,7 +29,7 @@ legend()
 figure(2)
 semilogy(time_gmres, res_gmres,color="black", linestyle="--",
         label=L"$\textnormal{GMRES}$")
-semilogy(time_cgn, res_cgn, color="red", linestyle="-",
+semilogy(time_cgn, res_cgn_2, color="red", linestyle="-",
         label=L"$\textnormal{CGN}$")
 xlabel(L"$\textnormal{CPU-time}$")
 ylabel(L"$\frac{||Ax-b||_2}{||b||_2}$")
