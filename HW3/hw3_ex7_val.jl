a54ddb1da431a6cde580047772754c3df32b03be
Simon Kern <skern@mech.kth.se>
2020-12-10 23:09:17 +0100

    fixed ram-filler bug ;)

--- HW3/hw3_ex7_val.jl
+++ HW3/hw3_ex7_val.jl
@@ -1,22 +1,27 @@
 # Exercise 7 - HW3
-using Pkg
-Pkg.add("QuadGK")
-Pkg.add("SymPy")
-Pkg.add("MAT")
-Pkg.add("MatrixDepot")
+#using Pkg
+#Pkg.add("QuadGK")
+#Pkg.add("SymPy")
+#Pkg.add("MAT")
+#Pkg.add("MatrixDepot")
 using LinearAlgebra, QuadGK, SymPy, MatrixDepot, MAT, SparseArrays
 
 function gramian_val(A, B, t, tol)
+    n = size(A,1)
     err = 1;
     i = 0;
+    P = spzeros(n,n);
+    err_e = 0;
     while err > tol
         T = t^(i+1)/factorial(i+1);
+        C = spzeros(n,n)
         for k = 0:i
-            C += (-1)^k*factorial(i)/(factorial(k)*factorial(i-k))*(A^k*B*A^(i-k));
+            fact = (-1)^k*factorial(i)/(factorial(k)*factorial(i-k))
+            C += fact*(A^k*B*A^(i-k));
         end
         P += T*C;
         err_e += 1/factorial(i);
-        err = norm(B)*(e-err_e)
+        err = norm(B)*(exp(1)-err_e)
         i += 1;
     end
     return P
@@ -24,15 +29,15 @@ end
 
 tau = 1;
 tol = 1e-10;
-A = matrixdepot("neumann", 20^2)
+A = matrixdepot("neumann", 30)
 A = A-A'; # make A antisymmetric
 A = A/(2*norm(A,1));
 B = sprandn(size(A,1), size(A,1), 0.05);
 
-myapp = @timed P = commutator_val(A, B, tau, tol);
+myapp = @timed P = gramian_val(A, B, tau, tol);
 
 # naive approach
-f = t->exp(-t*A)*B*exp(t*A);
+f(t) = exp(collect(-t*A))*B*exp(collect(t*A));
 naive = @timed P_naive = quadgk(f, 0, tau)
 
 println("Time for naive is ", naive.time,
